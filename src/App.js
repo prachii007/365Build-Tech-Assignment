a1de2d8b1571b5900c753bb13bda36c6b553602e
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-12-05 17:46:01 +0530

    design navbar component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Home from '../src/pages/home';
 import Blog from '../src/pages/blog';
 import Users from '../src/pages/users';
 import './App.css';
+import Navbar from './pages/components/navbar';
 
 function App() {
   return (
     <HashRouter>
+      <Navbar/>
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/blog' element={<Blog />} />
