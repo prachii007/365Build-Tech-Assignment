051cc534b1f8dcab88ac1131662736f63310080a
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-12-06 00:21:29 +0530

    create and design blogCard component to display blog entries with user names

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,8 +1,63 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import BlogCard from './components/blogCard';
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [users, setUsers] = useState([]);
+  const combinedEntries = [];
+
+  const getBlogs = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(blogArray => {
+        setBlogs(blogArray);
+      })
+    console.log("blogs", blogs)
+  }
+
+  const getUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(userArray => {
+        setUsers(userArray);
+      })
+    console.log("users", users)
+  }
+
+  for (let i = 0; i < blogs.length; i++) {
+    const index = Math.floor(i / 10);
+    if (users[index]) {
+      combinedEntries.push({ ...blogs[i], ...users[index] });
+    } else {
+      combinedEntries.push(blogs[i]);
+    }
+  }
+  console.log("combine", combinedEntries)
+  useEffect(() => {
+    getBlogs();
+    getUsers();
+  }, [1]);
+
   return (
-    <div>Blog</div>
+    <div className='container'>
+      <div className='my-3 d-flex justify-content-between'>
+        <h2 className=''>List of Blogs</h2>
+        <button className='btn btn-primary'>
+          <svg width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" role="img" focusable="false">
+            <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"></path>
+            <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"></path>
+          </svg>
+        </button>
+      </div>
+      <div className='border rounded'>
+        {combinedEntries.map((entry, index) => {
+          return (
+            <BlogCard entry={entry} index={index} />
+          )
+        })}
+      </div>
+    </div>
   )
 }
 export default Blogs;
--- src/pages/components/blogCard.js
+++ src/pages/components/blogCard.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-export const BlogCard = () => {
+const BlogCard = ({entry, index}) => {
   return (
-    <div>BlogCard</div>
+    <div>
+      <div key={index} className='border-bottom mt-1 p-2'>
+        <div className='d-flex justify-content-between'>
+          <h5 className='mb-1'>{entry.title}</h5>
+          <small>
+            <a href='d'>View</a>
+          </small>
+        </div>
+        <p>{entry.body}</p>
+        <div className='text-end'>
+          <a className='text-body-secondary'>By: {entry.name}</a>
+        </div>
+      </div>
+    </div>
   )
 }
+export default BlogCard;
