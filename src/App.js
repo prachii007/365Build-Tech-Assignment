c3856d7645ab94808885add05e20d85867a26c94
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-12-05 15:48:03 +0530

    initialise project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from '../src/pages/home';
+import Blog from '../src/pages/blog';
+import Users from '../src/pages/users';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/blog' element={<Blog />} />
+        <Route exact path='/users' element={<Users />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
