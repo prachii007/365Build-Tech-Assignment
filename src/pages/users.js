940b88eb357c0024b1caf1098e3ab06f9a60c701
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-12-06 12:41:58 +0530

    create and design user page

--- src/pages/users.js
+++ src/pages/users.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Users = () => {
+
+  const [users, setUsers] = useState([]);
+
+  const getUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(Response => Response.json())
+      .then(userArray => setUsers(userArray))
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [1])
+
   return (
-    <div>Users</div>
+    <div className='container'>
+      <h1 className='my-3'>Users</h1>
+      <div className='list-group'>
+        {users.map((item, index) => {
+          return (
+            <a className='list-group-item' key={index} href="hh">
+              {item.name}
+            </a>
+          )
+        })
+        }
+      </div>
+
+    </div>
   )
 }
 export default Users;
