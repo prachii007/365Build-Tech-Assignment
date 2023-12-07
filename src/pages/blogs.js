18e57b382c64171bef7888f74036b22d3cd1e1f4
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-12-07 12:46:50 +0530

    make the view button navigate to the individual post page of each user

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import BlogCard from './components/blogCard';
+import { useNavigate } from 'react-router-dom';
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
   const [users, setUsers] = useState([]);
   const combinedEntries = [];
+  const navigate = useNavigate()
 
   const getBlogs = () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -31,6 +33,11 @@ const Blogs = () => {
       combinedEntries.push(blogs[i]);
     }
   }
+
+  const goToSinglePostPage = (entry, index) => {
+    navigate(`/individualpost/${parseInt(entry.userId) - 1}/${index % 10}`)
+  }
+
   useEffect(() => {
     getBlogs();
     getUsers();
@@ -43,14 +50,14 @@ const Blogs = () => {
         <button className='btn btn-primary'>
           <svg width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" role="img" focusable="false">
             <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"></path>
-            <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"></path>
+            <path fillRule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"></path>
           </svg>
         </button>
       </div>
       <div className='list-group'>
         {combinedEntries.map((entry, index) => {
           return (
-            <BlogCard entry={entry} index={index} />
+            <BlogCard entry={entry} index={index} goToSinglePostPage={goToSinglePostPage} />
           )
         })}
       </div>
--- src/pages/components/blogCard.js
+++ src/pages/components/blogCard.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const BlogCard = ({ entry, index }) => {
+const BlogCard = ({ entry, index, goToSinglePostPage }) => {
   return (
     <div key={index} className='list-group-item list-group-item-action'>
       <div className='d-flex justify-content-between'>
         <h5 className='mb-1'>{entry.title}</h5>
-        <small>
-          <a href='d'>View</a>
-        </small>
+        <button onClick={goToSinglePostPage.bind(this, entry, index)} className='btn text-decoration-underline link-underline-primary text-primary'><small>View</small></button>
       </div>
       <p className='mb-1'>{entry.body}</p>
       <div className='d-flex justify-content-end'>
