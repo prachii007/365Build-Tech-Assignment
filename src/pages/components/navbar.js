87e16ef9fcc0bace87813efca9c5647587df7073
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-12-07 14:25:03 +0530

    replace Link with NavLink

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink  } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme='dark'>
       <div className="container-fluid">
-        <Link className="navbar-brand" to='/'>Blogs App</Link>
+        <NavLink  className="navbar-brand" to='/'>Blogs App</NavLink >
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <NavLink  className="nav-link"  activeClassName="active" aria-current="page" to="/">Home</NavLink >
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/blogs">Blogs</Link>
+              <NavLink  className="nav-link"  activeClassName="active" to="/blogs">Blogs</NavLink >
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/users">Users</Link>
+              <NavLink  className="nav-link"  activeClassName="active" to="/users">Users</NavLink >
             </li>
           </ul>
           <form className="d-flex" role="search">
